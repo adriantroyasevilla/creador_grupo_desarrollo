21befc65ff49c8cefa668c81ecfb94b1adad55d7
Jean Michael Courtois <atsvex94@gmail.com>
2019-07-10 14:49:59 +0200

    Commit inicial

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<!-- HTML5 --> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio</title>
+    <!-- links externos -->
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Fontawesome -->
+</head>
+
+<body>
+
+<!-- HEADER -->
+    <header>
+      <h1>CREADOR GRUPO DESARROLLO</h1>
+    </header>
+
+<!-- SECTION -->
+<section>
+    
+</section>
+
+</body>
+</html>
